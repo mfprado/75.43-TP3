44f3303722325eaf409c4a83f01512d965904c93
mariaflorenciaprado <pradomflorencia@gmail.com>
2019-06-17 11:10:29 -0300

    Script default argument value

--- runTopology.sh
+++ runTopology.sh
@@ -1,2 +1,3 @@
-docker-compose exec mininet mn --custom /tmp/topology/datacenter.py --topo datacenter,$1  --mac --arp --switch ovsk --controller remote
+levels=${1:-1}
+docker-compose exec mininet mn --custom /tmp/topology/datacenter.py --topo datacenter,$levels  --mac --arp --switch ovsk --controller remote
 
