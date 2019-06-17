117e2d96f5a1fcb24850ab8ac269ac1b31bddcbe
mariaflorenciaprado <pradomflorencia@gmail.com>
2019-06-17 01:44:57 -0300

    Fix topology

--- runTopology.sh
+++ runTopology.sh
@@ -1,2 +1,2 @@
-docker-compose exec mininet mn --custom /tmp/topology/datacenter.py --topo datacenter --mac --arp --switch ovsk --controller remote
+docker-compose exec mininet mn --custom /tmp/topology/datacenter.py --topo datacenter  --mac --arp --switch ovsk --controller remote
 
--- topology/datacenter.py
+++ topology/datacenter.py
@@ -10,15 +10,17 @@ from mininet.topo import Topo
 class Datacenter( Topo ):
 
   def create_switches(self, levels):
+    print("Initializing Switches")
     switches_by_level = {} 
     for level in range(levels):
        level_switches = []
-       for switch in range(level**2):
+       for switch in range(2**level):
          level_switches.append(self.addSwitch('sw1%d_%d'%(level,switch)))
        switches_by_level[level] = level_switches
     return switches_by_level
 
   def add_links_to_switches(self, switches_by_level):
+    print("Initializing Links")
     for level, switches in switches_by_level.items():
       if level > 0:
         for switch1 in switches_by_level[level-1]:
@@ -26,6 +28,13 @@ class Datacenter( Topo ):
             self.addLink(switch1, switch2)
   
   def add_hosts_to_network(self, switches_by_level):
+    root_switch = switches_by_level[0][0]
+    print("Initializing Clients")
+    for i in range(3):
+      client = self.addHost('client%s'%i)
+      self.addLink(root_switch, client)
+
+    print("Initializing Providers")
     last_level = len(switches_by_level)-1
     for i, switch in enumerate(switches_by_level[last_level]):
       host = self.addHost('h%s'%i)
@@ -33,9 +42,9 @@ class Datacenter( Topo ):
 
 
 
-  def __init__( self, levels = 5, half_ports = 2, **opts ):
+  def __init__( self, levels = 1, half_ports = 2, **opts ):
     Topo.__init__(self, **opts)
-    print("hola")
+    print("Initializing DataCenter Topology\nLevels: %s"%levels)
 
     switches_by_level = self.create_switches(levels)
 
